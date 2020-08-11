1b11d6cf37cc199d6c3fdf8d2ed4780a3f0d0261
zhuxingmin <xmzhu@che300.com>
2020-08-11 11:43:56 +0800

    修改配置文件

--- .umirc.ts
+++ .umirc.ts
@@ -4,38 +4,45 @@ const Dev = process.env.NODE_ENV === 'development';
 
 // more config: https://d.umijs.org/config
 export default defineConfig({
-  // mode: 'site',
-  favicon: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
-  logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
-  title: '车300组件文档',
-  description: '车300-前端',
-  outputPath: 'docs-dist',
-  // exportStatic: {
-  //   htmlSuffix: true
-  // },
-  resolve: {
-    previewLangs: ['js', 'jsx', 'ts', 'tsx'],
-  },
-  publicPath: Dev ? '/' : './',
-  ignoreMomentLocale: true,
-  base: Dev ? '/' : '/dumi/',
-  // dynamicImport: {},
-  extraBabelPlugins: [
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
-  ],
-  styles: [
-    `th[align='center'] {
+    // mode: 'site',
+    favicon: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+    logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+    title: '车300组件文档',
+    description: '车300-前端',
+    outputPath: 'docs-dist',
+    // exportStatic: {
+    //   htmlSuffix: true
+    // },
+    resolve: {
+        previewLangs: ['js', 'jsx', 'ts', 'tsx'],
+    },
+    publicPath: Dev ? '/' : './',
+    ignoreMomentLocale: true,
+    base: Dev ? '/' : '/dumi/',
+    // dynamicImport: {},
+    extraBabelPlugins: [
+        [
+            'import',
+            { libraryName: 'antd', libraryDirectory: 'es', style: true },
+        ],
+    ],
+    styles: [
+        `th[align='center'] {
       text-align: center;
     }`,
-  ],
-  // links: [
-  //   {
-  //     href: Dev ? '../common.css' : './common.css',
-  //     rel: 'stylesheet',
-  //   },
-  // ],
-  externals: {
-    moment: 'moment',
-  },
-  scripts: ['https://cdn.bootcdn.net/ajax/libs/moment.js/2.27.0/moment.min.js'],
+    ],
+    // links: [
+    //   {
+    //     href: Dev ? '../common.css' : './common.css',
+    //     rel: 'stylesheet',
+    //   },
+    // ],
+    externals: {
+        moment: 'moment',
+        React: 'react',
+    },
+    scripts: [
+        'https://cdn.bootcdn.net/ajax/libs/moment.js/2.27.0/moment.min.js',
+        'https://cdn.bootcdn.net/ajax/libs/react/16.12.0/umd/react.production.min.js',
+    ],
 });
